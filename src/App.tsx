48a7fa33d7d6a4ef636f7e9d9bcdaa23b80736b9
David Andersson <david.andersson@zocom.se>
2022-11-22 11:24:09 +0100

    Cleaned files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+function App() {
+
+	return (
+		<div className="App">
+			<header>
+				Header
+			</header>
+			<main>
+				Main
+			</main>
+		</div>
+	)
+}
+
+export default App
